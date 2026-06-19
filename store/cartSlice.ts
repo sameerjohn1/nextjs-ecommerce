0ecc90dffd71554db35aa46a4545d140a681bb84
sameerjohn1 <sameerprogrammer5@gmail.com>
2026-06-19 09:34:39 +0500

    feat: improve addToCart reducer logic with typing

--- store/cartSlice.ts
+++ store/cartSlice.ts
@@ -20,7 +20,18 @@ const initialState:ICarState={
 const cartSlice=createSlice({
     initialState,
     name:"cart",
-    reducers:{},
+    reducers:{
+        addToCart:(state,action)=>{
+            const {product,quantity=1}=action.payload;
+            const existingStateIndex=state.items.findIndex((items)=>items.product.id===product.id)
+
+            if(existingStateIndex>=0){
+                state.items[existingStateIndex].quantity+=quantity
+            }else{
+            state.items.push({product,quantity})
+            }
+        },   
+    },
 })
 
 export default cartSlice.reducer
